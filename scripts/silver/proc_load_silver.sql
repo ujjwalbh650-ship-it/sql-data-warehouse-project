b364235ac9c265a6a671fe7b300c46c8a25e8011
ujjwalbh650-ship-it <ujjwalbh650@gmail.com>
2026-07-16 23:40:20 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -11,7 +11,7 @@ Script Purpose:
 		
 Parameters:
     None. 
-	  This stored procedure does not accept any parameters or return any values.
+	  This stored procedure does not accept any parameters or return any values. 
 
 Usage Example:
     EXEC Silver.load_silver;
